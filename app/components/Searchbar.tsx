2400dd847b18ad58221080c49d9add69789ca2e0
Illya-01 <illyaroman103@gmail.com>
2025-07-02 17:47:58 +0300

    feat: add searchbar and backgorund image to main screen

---
+++ app/components/Searchbar.tsx
@@ -0,0 +1,30 @@
+import { icons } from "@/constants/icons";
+import React from "react";
+import { Image, TextInput, View } from "react-native";
+
+interface SearchbarProps {
+  onPress?: () => void;
+}
+
+const Searchbar = ({ onPress }: SearchbarProps) => {
+  return (
+    <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
+      <Image
+        source={icons.search}
+        className="size-5"
+        resizeMode="contain"
+        tintColor="#AB8BFF"
+      />
+      <TextInput
+        value=""
+        placeholder="Search through 300+ movies online"
+        placeholderTextColor="#A8B5DB"
+        onPress={onPress}
+        onChangeText={() => {}}
+        className="flex-1 ml-2 text-white"
+      />
+    </View>
+  );
+};
+
+export default Searchbar;
