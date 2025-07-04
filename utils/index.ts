6e56ec532bb4f72a433d5ad7a8a894ecad1ceec8
Illya-01 <illyaroman103@gmail.com>
2025-07-04 23:54:25 +0300

    fix: correct runtime formatting logic in convertToHoursAndMinutes function

--- utils/index.ts
+++ utils/index.ts
@@ -20,6 +20,15 @@ export const convertToHoursAndMinutes = (runtime: number) => {
 
   const hours = Math.floor(runtime / 60);
   const minutes = runtime % 60;
+
+  if (hours === 0) {
+    return `${minutes}m`;
+  }
+
+  if (minutes === 0) {
+    return `${hours}h`;
+  }
+
   return `${hours}h ${minutes}m`;
 };
 
